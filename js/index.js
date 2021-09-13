f96d568193006decae30bad9100f653b7fbac871
kuangjunhua <1484427690@qq.com>
2021-09-13 20:24:28 +0800

    commit 0913

--- js/index.js
+++ js/index.js
@@ -5,10 +5,47 @@ let n=0
 let left=0;
 setInterval(()=>{
   left=-1226*n;
-  console.log(left);
+  //console.log(left);
   outer.style.marginLeft=left+"px";
   n++;
   if(n==5){
     n=0;
   }
 },3000);
+
+let phones=document.querySelectorAll("#main #phone .item");
+for(let i=1;i<=phones.length;i++){
+  phones[i-1].onclick=function(){
+    window.location.href="/detail.html?type=phone&id="+i;
+  }
+}
+let jiadians=document.querySelectorAll("#main .jiadian .item");
+for(let i=1;i<=jiadians.length;i++){
+  jiadians[i-1].onclick=function(){
+    window.location.href="/detail.html?type=jiadian&id="+i;
+  }
+}
+let zinengs=document.querySelectorAll("#main .zineng .item");
+for(let i=1;i<=zinengs.length;i++){
+  zinengs[i-1].onclick=function(){
+    window.location.href="/detail.html?type=zineng&id="+i;
+  }
+}
+let dapeis=document.querySelectorAll("#main .dapei .item");
+for(let i=1;i<=dapeis.length;i++){
+  dapeis[i-1].onclick=function(){
+    window.location.href="/detail.html?type=dapei&id="+i;
+  }
+}
+let peijians =document.querySelectorAll("#main .peijian .item");
+for(let i=1;i<=peijians.length;i++){
+  peijians[i-1].onclick=function(){
+    window.location.href="/detail.html?type=peijian&id="+i;
+  }
+}
+let zhoubians=document.querySelectorAll("#main .zhoubian .item");
+for(let i=1;i<=zhoubians.length;i++){
+  zhoubians[i-1].onclick=function(){
+    window.location.href="/detail.html?type=zhoubian&id="+i;
+  }
+}
--- js/login.js
+++ js/login.js
@@ -1,5 +1,9 @@
+
 let look=document.querySelector("#right>.mid form>div>span")
 let upwd=document.getElementById("upwd");
+let uname=document.getElementById("uname");
+let login_btn=document.getElementById("login_btn");
+//查看密码
 look.onclick=function(){
   if(look.className=='unlook'){
     upwd.type="text";
@@ -9,3 +13,27 @@ look.onclick=function(){
     look.className="unlook";
   }
 }
+//登陆
+uname.onblur=function(){
+  if(!uname.value){
+    alert("用户名不能为空");
+  }
+}
+upwd.onblur=function(){
+  if(!upwd.value){
+    alert("密码不能为空");
+  }
+}
+let login=function(e){
+  e.preventDefault();
+  if(!uname.value || !upwd.value){
+    return;
+  }
+  console.log(uname.value)
+  console.log(upwd.value)
+  axios.get("http://127.0.0.1:3000/login",{params:{uname:uname.value,upwd:upwd.value}})
+  .then(res=>{
+    console.log(res.data);
+  })
+}
+login_btn.addEventListener('click',login)
